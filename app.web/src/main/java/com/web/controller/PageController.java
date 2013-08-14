3b0a3e6a120b086ab5e85cffe077cb7ec3ccd686
swadhin <swadhin4@gmail.com>
2013-08-15 04:27:23 +0530

    PageController for JSP Files included

---
+++ app.web/src/main/java/com/web/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class PageController extends BaseController {
+
+  @RequestMapping("/page1")
+  public String page1Example(){
+    return "test";
+  }
+
+  @RequestMapping("/page2")
+  public String page2Example(){
+    return "test2";
+  }
+}
